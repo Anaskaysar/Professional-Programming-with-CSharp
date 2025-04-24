7e73dd820c3249c66c9a2c031cd2cc2baf482157
anaskaysar <kaysarulanas2@gmail.com>
2025-04-24 11:16:42 -0400

    updates

--- CodeExamples/Operators/obj/Debug/net9.0/Operators.AssemblyInfo.cs
+++ CodeExamples/Operators/obj/Debug/net9.0/Operators.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Operators")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f93a40964ed1c2534d633c64e71e53d94701973e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Operators")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Operators")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
